d55fab08737f6b0ae90815427ad844bebb0ff835
AllenHunn <allenhunn@gmail.com>
2019-03-14 12:38:42 -0400

    added type

--- src/app/cat/dto/baseDTO.ts
+++ src/app/cat/dto/baseDTO.ts
@@ -2,8 +2,8 @@ import { transformAndValidate } from 'class-transformer-validator';
 
 export type StaticThis<T> = { new(): T };
 
-export class BaseDTO {
-  static async createValidDTO<T extends BaseDTO>(this: StaticThis<T>, pojo: Object) {
+export abstract class BaseDTO {
+  static async createValidDTO<T extends BaseDTO>(this: StaticThis<T>, pojo: Object): Promise<T> {
     return await transformAndValidate(this, pojo);
   }
 }
